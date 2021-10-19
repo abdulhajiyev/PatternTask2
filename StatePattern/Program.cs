e90f057503c9c785cd820b753084350039873f20
Abdul Hajiyev <abdulhajiyev@gmail.com>
2021-10-20 02:47:34 +0400

    Strategy Pattern

--- StatePattern/Program.cs
+++ StatePattern/Program.cs
@@ -2,11 +2,73 @@
 
 namespace StatePattern
 {
-    class Program
+    public static class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Player player = new();
+            player.BulletHit(3);
+            player.BulletHit(9);
+            player.BulletHit(12);
+
+            Console.ReadKey();
+        }
+    }
+
+    // Context class
+    public class Player
+    {
+        IState _currentState;
+
+        public Player()
+        {
+            _currentState = new HealthyState();
+        }
+
+        public void BulletHit(int bullets)
+        {
+            Console.WriteLine($"{bullets} bullets hit the player");
+            _currentState = bullets switch
+            {
+                < 5 => new HealthyState(),
+                >= 5 and < 10 => new HurtState(),
+                >= 10 => new DeadState()
+            };
+
+            _currentState.ExecuteCommand(this);
+        }
+    }
+
+    // State interface
+    public interface IState
+    {
+        void ExecuteCommand(Player player);
+    }
+
+    // 'ConcreteStateA' class
+    public class HealthyState : IState
+    {
+        public void ExecuteCommand(Player player)
+        {
+            Console.WriteLine("The player is healthy\n");
+        }
+    }
+
+    // ConcreteStateB class
+    public class HurtState : IState
+    {
+        public void ExecuteCommand(Player player)
+        {
+            Console.WriteLine("The player is wounded. Please search health points\n");
+        }
+    }
+
+    // ConcreteStateC class
+    public class DeadState : IState
+    {
+        public void ExecuteCommand(Player player)
+        {
+            Console.WriteLine("The player is dead. Game Over");
         }
     }
 }
--- StrategyPattern/Program.cs
+++ StrategyPattern/Program.cs
@@ -2,11 +2,66 @@
 
 namespace StrategyPattern
 {
-    class Program
+    internal static class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            IAttack strategy = new AssaultRifle();
+            AttackStrategy soldier = new(strategy);
+            soldier.Attack();
+
+            strategy = new Shotgun();
+            soldier = new(strategy);
+            soldier.Attack();
+
+            strategy = new SubmachineGun();
+            soldier = new(strategy);
+            soldier.Attack();
+
+            Console.ReadLine();
+        }
+    }
+
+    // IStrategy interface
+    public interface IAttack
+    {
+        void Attack();
+    }
+
+    // ConcreteStrategyA class
+    public class AssaultRifle : IAttack
+    {
+        // Implementing A strategy
+        public void Attack() => Console.WriteLine("Attacked with Assault Rifle");
+    }
+
+    // ConcreteStrategyB class
+    public class Shotgun : IAttack
+    {
+        // Implementing B strategy
+        public void Attack() => Console.WriteLine("Attacked with Shotgun");
+    }
+
+    // ConcreteStrategyC class
+    public class SubmachineGun : IAttack
+    {
+        // Implementing C strategy
+        public void Attack() => Console.WriteLine("Attacked with Submachine Gun");
+    }
+
+    // Context class
+    public class AttackStrategy
+    {
+        private IAttack _strategy;
+
+        public AttackStrategy(IAttack strategy)
+        {
+            _strategy = strategy;
+        }
+
+        public void Attack()
+        {
+            _strategy.Attack();
         }
     }
 }
